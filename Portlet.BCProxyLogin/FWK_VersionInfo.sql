ccee22f3d21c6dda03be1e60209a0e0f7813d007
iamwyza <iamwyza@gmail.com>
2012-09-14 20:35:16 -0400

    updated to version 1.2

--- Portlet.BCProxyLogin/FWK_VersionInfo.sql
+++ Portlet.BCProxyLogin/FWK_VersionInfo.sql
@@ -1,8 +1,8 @@
 begin tran 
 use ICS_NET; 
 Declare @ApplicationName varchar(255) = 'CUS BC Proxy Login Portlet'; 
-Declare @VersionNumber varchar(255) = '1.12'; 
-Declare @VersionDate DateTime = '2012-06-18 09:50:00';
+Declare @VersionNumber varchar(255) = '1.2'; 
+Declare @VersionDate DateTime = '2012-09-14 09:50:00';
 if exists (select * from [FWK_VersionInfo] where [ApplicationName] = @ApplicationName) 
 begin 
    update [FWK_VersionInfo] set VersionNumber = @VersionNumber, VersionDate = @VersionDate 
